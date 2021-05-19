28a9fcba4ef1b2e1d23ac166a73ebbb30e52e6ac
Andrew Rosenfrisk <andrew.rosenfrisk@gmail.com>
2021-05-19 17:01:36 -0500

    Implement user registration
    Added Argon2 dependency, implemented AuthService and /Auth/Register
    route.

--- src/auth/api/routes/auth.js
+++ src/auth/api/routes/auth.js
@@ -1,11 +1,28 @@
 const { Router } = require("express");
+const { User } = require("../../models");
+const { AuthService } = require("../../services");
 
 const router = Router();
+const authService = new AuthService(User);
 
 module.exports = (routes) => {
   routes.use("/auth", router);
 
   router.get("/", (req, res) => {
     res.status(200).json({ status: "success", message: "get /auth" });
+  });
+
+  router.post("/register", async (req, res, next) => {
+    try {
+      res.status(201).json({
+        status: "success",
+        message: "User registered!",
+        data: {
+          user: await authService.register(req.body),
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
   });
 };
