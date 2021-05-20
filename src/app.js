8e2e662dc08776fa952c89de8c5d106e03e12dc4
Andrew Rosenfrisk <andrew.rosenfrisk@gmail.com>
2021-05-20 12:38:09 -0500

    Add route validation
    Dependency on Celebrate

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const cors = require("cors");
 require("pkginfo")(module, "name");
 const express = require("express");
+const { isCelebrate } = require("celebrate");
 const cookieParser = require("cookie-parser");
 
 // App initialization
@@ -20,6 +21,23 @@ app.use((req, res, next) => {
   const err = new Error("Not Found!");
   err.status = 404;
   next(err);
+});
+
+// Error handler registration.
+
+app.use((err, req, res, next) => {
+  const status = isCelebrate(err) ? 400 : err.status || 500;
+  const message =
+    config.app.env === "production" && err.status === 500
+      ? "Something Went Wrong!"
+      : err.message;
+
+  if (status === 500) console.log(err.stack);
+
+  res.status(status).json({
+    status: status >= 500 ? "error" : "fail",
+    message,
+  });
 });
 
 module.exports = app;
--- src/auth/api/routes/auth.js
+++ src/auth/api/routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { celebrate, Joi } = require("celebrate");
 const { User } = require("../../models");
 const config = require("../../../config");
 const { AuthService } = require("../../services");
@@ -14,39 +15,58 @@ module.exports = (routes) => {
     res.status(200).json({ status: "success", message: "get /auth" });
   });
 
-  router.post("/register", async (req, res, next) => {
-    try {
-      res.status(201).json({
-        status: "success",
-        message: "User registered!",
-        data: {
-          user: await authService.register(req.body),
-        },
-      });
-    } catch (err) {
-      next(err);
+  router.post(
+    "/register",
+    celebrate({
+      body: Joi.object().keys({
+        name: Joi.string().trim().required(),
+        email: Joi.string().email().trim().required(),
+        password: Joi.string().required(),
+      }),
+    }),
+    async (req, res, next) => {
+      try {
+        res.status(201).json({
+          status: "success",
+          message: "User registered!",
+          data: {
+            user: await authService.register(req.body),
+          },
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
-  router.post("/login", async (req, res, next) => {
-    try {
-      const { accessToken, refreshToken } = await authService.login(req.body);
+  router.post(
+    "/login",
+    celebrate({
+      body: Joi.object().keys({
+        email: Joi.string().email().trim().required(),
+        password: Joi.string().required(),
+      }),
+    }),
+    async (req, res, next) => {
+      try {
+        const { accessToken, refreshToken } = await authService.login(req.body);
 
-      res.cookie(
-        "refreshToken",
-        refreshToken,
-        config.auth.refreshToken.cookie.options
-      );
+        res.cookie(
+          "refreshToken",
+          refreshToken,
+          config.auth.refreshToken.cookie.options
+        );
 
-      res.status(200).json({
-        status: "success",
-        message: "User Logged In!",
-        accessToken,
-      });
-    } catch (err) {
-      next(err);
+        res.status(200).json({
+          status: "success",
+          message: "User Logged In!",
+          accessToken,
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
   router.post("/refresh", (req, res, next) => {
     try {
